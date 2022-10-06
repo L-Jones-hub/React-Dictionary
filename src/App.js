61dffa1c06e74a837a8f7298f749800d9b5d218e
L-Jones-hub <105738902+L-Jones-hub@users.noreply.github.com>
2022-10-05 19:37:23 -0500

    Remove Lazy Loading

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
 import "./App.css";
 import Dictionary from "./Dictionary";
-import React, { Suspense } from "react";
-
-const Meaning = React.lazy(() => import("./Meaning"));
-const Phonetic = React.lazy(() => import("./Phonetic"));
-const Photos = React.lazy(() => import("./Photos"));
-const Results = React.lazy(() => import("./Results"));
-const Synonyms = React.lazy(() => import("./Synonyms"));
 
 export default function App() {
   return (
     <div>
-      <div>
-        <Suspense fallback={<div>Loading...</div>}>
-          <section>
-            <Meaning />
-            <Phonetic />
-            <Photos />
-            <Results />
-            <Synonyms />
-          </section>
-        </Suspense>
-      </div>
+      
       <div className="App">
         <h1>Dictionary App</h1>
         <div className="container">
